16da9fc7d57f44c215254a36a3ab8b3b0efb779c
die-perez <pzandie@gmail.com>
2021-05-16 16:04:05 -0700

    prompt and staging area logic

--- app.js
+++ app.js
@@ -1,4 +1,36 @@
-var str = "Alphabet";
-var res = str.split("");
-console.log(res)
+window.addEventListener("DOMContentLoaded", () => {
+    let keywordHoldingArea = document.querySelector("#wordToGuess")
+
+    // create prompt for keyword
+    var keyword = prompt("Player one, type in a keyword")
+    
+    // check for alphabet only (no numbers or characters)
+    while (!keyword || !keyword.match(/^[a-z]+$/)) {
+        alert("Only letters are allowed, no spaces or special characters.");
+        var keyword = prompt("Player one, type in a keyword")
+    }
+
+    // convert into array for comparison
+    var arr = keyword.split("")
+    console.log(arr)
+
+    // have number empty slots on screen display keyword length
+    function pushKeywordToMain(){
+        for (i=0; i< arr.length; i++){
+            var div = document.createElement("div")
+            div.innerText = (arr[i])
+            keywordHoldingArea.appendChild(div)
+            div.classList.add("border")
+            div.classList.add("hidden")
+        }
+    }
+    
+    pushKeywordToMain()
+
+
+})
+
+
+
+
 
