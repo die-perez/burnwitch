b4f79c203fa2c4ce59ed1d33c10a82358a253e30
die-perez <pzandie@gmail.com>
2021-05-16 17:02:58 -0700

    added class Guess functionality removed prompt for testing

--- app.js
+++ app.js
@@ -1,32 +1,63 @@
 window.addEventListener("DOMContentLoaded", () => {
+    // global event listeners
     let keywordHoldingArea = document.querySelector("#wordToGuess")
-
+    
+    
     // create prompt for keyword
-    var keyword = prompt("Player one, type in a keyword")
+    // var keyword = prompt("Player one, type in a keyword")
     
     // check for alphabet only (no numbers or characters)
-    while (!keyword || !keyword.match(/^[a-z]+$/)) {
-        alert("Only letters are allowed, no spaces or special characters.");
-        var keyword = prompt("Player one, type in a keyword")
-    }
+    // while (!keyword || !keyword.match(/^[a-z]+$/)) {
+    //     alert("Only letters are allowed, no spaces or special characters.");
+    //     var keyword = prompt("Player one, type in a keyword")
+    // }
+
+    var keyword = "banana"
 
     // convert into array for comparison
-    var arr = keyword.split("")
+    let arr = keyword.split("")
     console.log(arr)
 
     // have number empty slots on screen display keyword length
     function pushKeywordToMain(){
         for (i=0; i< arr.length; i++){
-            var div = document.createElement("div")
+            let div = document.createElement("div")
             div.innerText = (arr[i])
             keywordHoldingArea.appendChild(div)
             div.classList.add("border")
             div.classList.add("hidden")
+            div.classList.add(arr[i])
         }
     }
-    
+
     pushKeywordToMain()
 
+
+    class Guess {
+        static wrongGuesses = 0
+        static rightGuesses = 0
+
+        constructor(letter){
+            this.letter = letter
+        }
+
+        compare(){
+            for(i=0; i<arr.length; i++){
+            console.log(arr[i])
+                if (this.letter == arr[i]) {
+                    let divs = Array.from(document.getElementsByClassName(this.letter))
+                    divs.forEach(element => {
+                        element.classList.remove("hidden")
+                    });
+                    
+                }
+            }
+        }
+
+    }
+
+    const guessOne = new Guess ('a')
+    guessOne.compare()
 
 })
 
